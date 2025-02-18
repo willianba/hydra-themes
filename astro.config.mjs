632b7ab26a9f5f366902f36cab02ec4a8db87c4b
Chubby Granny Chaser <hydralauncher@proton.me>
2025-02-18 01:02:09 +0000

    feat: adding ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  server: {
+    host: true,
+  },
   integrations: [tailwind({ applyBaseStyles: false }), react()],
-  adapter: cloudflare(),
+  adapter: node({
+    mode: "standalone",
+  }),
 });
