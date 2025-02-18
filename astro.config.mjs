cad8a9a778adc15b2e91fb0e4bb1bcea8890c72e
Chubby Granny Chaser <hydralauncher@proton.me>
2025-02-18 00:48:19 +0000

    feat: adding ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind({ applyBaseStyles: false }), react()],
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
 });
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,5 @@
 import { createClient } from "redis";
 
-console.log(import.meta.env.REDIS_URL);
-
 export const redis = createClient({
   url: import.meta.env.REDIS_URL,
 });
