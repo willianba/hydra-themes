1201765e24049c06a44512c868c2c592214397b0
Chubby Granny Chaser <hydralauncher@proton.me>
2025-05-09 09:51:05 +0100

    fix: adding fallback to api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import ky from "ky";
 
 export const api = ky.create({
-  prefixUrl: import.meta.env.PUBLIC_API_URL,
+  prefixUrl: import.meta.env.PUBLIC_API_URL || "https://api.hydrathemes.shop",
   credentials: "include",
 });
